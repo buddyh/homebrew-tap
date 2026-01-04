2af6151506ae09d8495c59d8ff98ff4575ec88ef
buddyh <31752869+buddyh@users.noreply.github.com>
2026-01-04 15:27:41 -0500

    Update todoist to v0.2.0

--- todoist.rb
+++ todoist.rb
@@ -4,28 +4,28 @@
 class Todoist < Formula
   desc "Todoist CLI - manage tasks from the command line"
   homepage "https://github.com/buddyh/todoist-cli"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/buddyh/todoist-cli/releases/download/v0.1.0/todoist_0.1.0_darwin_amd64.tar.gz"
-      sha256 "5aa2ba75f78ce8384302cddaa4846f20bde40fd8bf9386d4aef193e5501001e2"
+      url "https://github.com/buddyh/todoist-cli/releases/download/v0.2.0/todoist_0.2.0_darwin_amd64.tar.gz"
+      sha256 "55557edc3505a7ad4c1cc515fcde4bdeb45f38352e480d0f312e701db3077804"
     end
     on_arm do
-      url "https://github.com/buddyh/todoist-cli/releases/download/v0.1.0/todoist_0.1.0_darwin_arm64.tar.gz"
-      sha256 "9f7cdcf60a1b1dc3a82aa4ac9f8df8f1e397016aff34801159a48d3393234a1a"
+      url "https://github.com/buddyh/todoist-cli/releases/download/v0.2.0/todoist_0.2.0_darwin_arm64.tar.gz"
+      sha256 "13db35bfe632244a4a0530936813cafa313b6793ea19081753dcfb96048ab92d"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/buddyh/todoist-cli/releases/download/v0.1.0/todoist_0.1.0_linux_amd64.tar.gz"
-      sha256 "dda6ccfbe9f40a5b319dfc62b4fd5c073cecd52c67b1f340767277744dc02f50"
+      url "https://github.com/buddyh/todoist-cli/releases/download/v0.2.0/todoist_0.2.0_linux_amd64.tar.gz"
+      sha256 "37f89254908d7b926d6b5d5610d68b88a36676d17adad54f27364dee3c945a91"
     end
     on_arm do
-      url "https://github.com/buddyh/todoist-cli/releases/download/v0.1.0/todoist_0.1.0_linux_arm64.tar.gz"
-      sha256 "83dc7c0efd235985016d3d60986eae3559011f08684623c460458b0ddd1409aa"
+      url "https://github.com/buddyh/todoist-cli/releases/download/v0.2.0/todoist_0.2.0_linux_arm64.tar.gz"
+      sha256 "483478b80ec8cf3a29515695296dc35cfaae42e62f32285d2e0eda5d0e68ccaa"
     end
   end
 
